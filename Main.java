daf7119f685fa957f4c8c873d7aa5287975c849d
Mattia Gaetano Greco <101709398+Mattia-Gaetano-Greco@users.noreply.github.com>
2022-10-10 10:26:06 +0000

    Versione base: - Generazione di numeri casuali per ogni ruota

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("pisello");
+    Ruota ruote[] = new Ruota[3];
+    ruote[0] = new Ruota("Torino", 5);
+    ruote[1] = new Ruota("Milano", 5);
+    ruote[2] = new Ruota("Bari", 5);
+
+    for (int i = 0; i < 3; i++) {
+      ruote[i].start();
+    }
   }
 }
--- Routa.java
+++ Routa.java
@@ -1,4 +1,22 @@
-public class Ruota extends Thread {
-  private int numeri[] = {};
-  private int numeroCasuale
-} 
+import java.util.Random;
+
+class Ruota extends Thread {
+  private String nome;
+  private int nNumeri;
+
+  public Ruota(String nome, int nNumeri) {
+    this.nome = nome;
+    this.nNumeri = nNumeri;
+  }
+
+  public void run() {
+    for (int i = 0; i < nNumeri; i++) {
+      Random r = new Random();
+      System.out.println(nome + " " + (r.nextInt(90)+1));
+      try{
+        sleep(10);
+      }
+      catch(Exception e){}
+    }
+  }
+}
